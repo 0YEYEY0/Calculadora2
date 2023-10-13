598223cc268255182f461218024c10f7d97cfb33
Jason <jason0101@estudiantec.cr>
2023-10-13 15:48:30 -0600

    Se crea la interfaz de la calculadora y se conecta con el arbol de expresion

---
+++ src/main/java/com/example/calculadora2/Calculadora.java
@@ -0,0 +1,68 @@
+package com.example.calculadora2;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Calculadora extends Application {
+
+    private TextField textField;
+    private Label labelResultado;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Calculadora");
+
+        // Campo de texto
+        textField = new TextField();
+        textField.setPromptText("Escribe una expresión");
+        textField.setLayoutX(10);
+        textField.setLayoutY(10);
+
+        // Label
+        labelResultado = new Label("Resultado:");
+        labelResultado.setLayoutX(10);
+        labelResultado.setLayoutY(90);
+
+        // Botón
+        Button button1 = new Button("Calcular");
+        button1.setLayoutX(10);
+        button1.setLayoutY(50);
+
+        // Acción para el Botón
+        button1.setOnAction(e -> calcularExpresion());
+
+        // Pane
+        Pane pane = new Pane();
+        pane.getChildren().addAll(textField, labelResultado, button1);
+
+        // Crear la escena
+        Scene scene = new Scene(pane, 300, 200);
+
+        // Agregar la escena a la ventana principal
+        primaryStage.setScene(scene);
+
+        // Mostrar la ventana
+        primaryStage.show();
+    }
+
+    private void calcularExpresion() {
+        String expresion = textField.getText();
+        ArbolExpresion arbol = new ArbolExpresion(expresion);
+        double resultado = arbol.evaluar();
+
+        // Mostrar el resultado en el Label
+        labelResultado.setText("Resultado: " + resultado);
+    }
+}
+
+
+
